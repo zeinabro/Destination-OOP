c6dd39ec8c08e7383a50471bdba2fbdfe78173ae
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-09 15:41:32 +0100

    person class

--- classes/Person.js
+++ classes/Person.js
@@ -1,5 +1,18 @@
 class Person {
+    #bags
+    constructor(name,destination){
+        this.name=name
+        this.destination=destination
+        this.#bags=[]
+    }
 
+    addBag(bag){
+        this.#bags.push(bag)
+    }
+
+    getBags(){
+        return this.#bags
+    }
 
 }
 
