035282c0007853d060abef55d57062a9be1a7be1
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-09 14:58:37 +0100

    plane class

--- classes/Plane.js
+++ classes/Plane.js
@@ -1,5 +1,19 @@
 class Plane {
+    #passengers
+    constructor(company,origin,destination){
+        this.company=company;
+        this.origin=origin;
+        this.destination=destination;
+        this.#passengers=[]
+    }
 
+    getPassengers(){
+        return this.#passengers
+    }
+
+    addPassenger(passenger){
+        this.#passengers.push(passenger)
+    }
 
 }
 
